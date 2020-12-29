be9d6eb6d80491c544a35ef1f0e2f6d3354b6542
Binjita <binjita.baral@gmail.com>
2020-12-29 13:06:46 -0600

    asking user's gift name

--- Christmas/Santa.java
+++ Christmas/Santa.java
@@ -5,8 +5,19 @@ Using Console IO, allow him to ask the user what they want for Christmas. Store 
  Bonus: Continue to ask if the user enters an empty String
  */
 
+import java.util.Scanner;
+
 public class Santa {
     public static void main(String[] args) {
 
+        System.out.println("What you guys want for Christmas :");
+        Scanner scan = new Scanner(System.in);
+
+        String userInput = scan.nextLine();
+
+        if(userInput.equals("")){
+            System.out.println("Please enter your gift");
+            String userInput1 = scan.nextLine();
+        }
     }
 }

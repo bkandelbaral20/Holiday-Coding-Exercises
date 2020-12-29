3c984e436cc31644a8e6a41e0fcdf1ffa9513347
Binjita <binjita.baral@gmail.com>
2020-12-29 13:06:20 -0600

    asking user's gift name

---
+++ Christmas/Santa.java
@@ -0,0 +1,12 @@
+
+/*
+Create a new Java package called Christmas. Create a new Java class called Santa. Create your main method.
+Using Console IO, allow him to ask the user what they want for Christmas. Store the response in a String variable.
+ Bonus: Continue to ask if the user enters an empty String
+ */
+
+public class Santa {
+    public static void main(String[] args) {
+
+    }
+}
